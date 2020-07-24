188be5d34c431ed29dd55eec999498684f6bbdcf
Ian Hutchinson <ianhutch90@gmail.com>
2020-07-24 16:48:32 +0100

    Update routes.js

--- routes.js
+++ routes.js
@@ -10,10 +10,11 @@ const member = require('./controllers/member.js');
 router.get("/", dashboard.index);
 router.get("/dashboard", dashboard.index);
 router.get("/about", about.index);
-
-router.get('/member/:id/deleteassessment/:assessmentid', member.deleteAssessment);
 router.get('/member/:id', member.index);
 router.get('/dashboard/deletemember/:id', dashboard.deleteMember);
+router.post('/member/:id/addassessment', member.addAssessment);
 
+
+router.get('/member/:id/deleteassessment/:assessmentid', member.deleteAssessment);
 
 module.exports = router;
