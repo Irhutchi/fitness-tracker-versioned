2b5d99939075ba732354c256655e5bfe97719046
Ian Hutchinson <ianhutch90@gmail.com>
2020-07-24 16:40:08 +0100

    Update member.js

--- controllers/member.js
+++ controllers/member.js
@@ -2,17 +2,35 @@
 
 const logger = require('../utils/logger');
 const memberStore = require('../models/member-store.js');
-
+const uuid = require("uuid");
 
 const member = {
   index(request, response) {
     const memberId = request.params.id;
     logger.debug('Member id = ', memberId);
     const viewData = {
-      title: 'Member',
+      title: request.body.title,
       member: memberStore.getMember(memberId),
     };
     response.render('member', viewData);
+  },
+
+  addAssessment(request, response) {
+    const memberId = request.params.id;
+    const member = memberStore.getMember(memberId);
+    const newAssessment = {
+      id: uuid.v1(),
+      title: request.body.title,
+      weight: request.body.weight,
+      chest: request.body.chest,
+      thigh: request.body.thigh,
+      upperArm: request.body.upperArm,
+      waist: request.body.waist,
+      hips: request.body.hips,
+    };
+    logger.debug('New Song = ', newAssessment);
+    memberStore.addAssessment(memberId, newAssessment);
+    response.redirect('/member/' + memberId);
   },
   
   deleteAssessment(request, response) {
@@ -20,7 +38,7 @@ const member = {
     const assessmentId = request.params.assessmentid;
     logger.debug(`Deleting Assessment ${assessmentId} from Member Profile ${memberId}`);
     memberStore.removeAssessment(memberId, assessmentId);
-    response.redirect('/member/' + memberId);
+    response.redirect("/member/" + memberId);
   }
 };
 
