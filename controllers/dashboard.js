92f9121f25d4bb7beac3eb49e76bf6052b0d8da4
Ian Hutchinson <ianhutch90@gmail.com>
2020-07-24 16:39:38 +0100

    Update dashboard.js

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,12 +1,14 @@
 "use strict";
 
 const logger = require("../utils/logger");
-const memberStore = require('../models/member-store.js'); 
+const memberStore = require('../models/member-store.js');
+const uuid = require("uuid");
 
 const dashboard = {
   index(request, response) {
     logger.info("dashboard rendering");
     const viewData = {
+      id: uuid.v1(),
       title: "BMI Tracker Dashboard",
       members: memberStore.getAllMembers(),
     };
